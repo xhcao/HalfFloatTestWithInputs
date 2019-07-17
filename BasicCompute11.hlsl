9ffdffad910dd044b7fbd090681ff37ed6882c7a
xhcao <xinghua.cao@intel.com>
2019-07-17 12:03:28 +0800

    Update BasicCompute11.hlsl

--- BasicCompute11.hlsl
+++ BasicCompute11.hlsl
@@ -10,44 +10,33 @@ RWStructuredBuffer<BufType> BufferOut : register(u0);
 [numthreads(1, 1, 1)]
 void CSMain( uint3 DTid : SV_DispatchThreadID )
 {
-	for (int i = 0; i < 10; i++)
+    for (int j = 0; j < 256; j++)
 	{
-		BufferOut[DTid.x].f[i] = Buffer0[DTid.x].f[i] * Buffer0[DTid.x].f[i]  * Buffer1[DTid.x].f[i] * Buffer1[DTid.x].f[i];
+	    for (int i = 0; i < 9; i++)
+	    {
+		    BufferOut[DTid.x * 256 + j].f[i] = Buffer0[DTid.x *256 + j].f[i] * Buffer1[DTid.x *256 + j].f[i];
+	    }
 	}
-}
-
-#else // The following code is for raw buffers
-
-ByteAddressBuffer Buffer0 : register(t0);
-ByteAddressBuffer Buffer1 : register(t1);
-RWByteAddressBuffer BufferOut : register(u0);
-
-[numthreads(1, 1, 1)]
-void CSMain( uint3 DTid : SV_DispatchThreadID )
-{
-#ifdef TEST_DOUBLE
-    int i0 = asint( Buffer0.Load( DTid.x*16 ) );
-    float f0 = asfloat( Buffer0.Load( DTid.x*16+4 ) );
-    double d0 = asdouble( Buffer0.Load( DTid.x*16+8 ), Buffer0.Load( DTid.x*16+12 ) );
-    int i1 = asint( Buffer1.Load( DTid.x*16 ) );
-    float f1 = asfloat( Buffer1.Load( DTid.x*16+4 ) );
-    double d1 = asdouble( Buffer1.Load( DTid.x*16+8 ), Buffer1.Load( DTid.x*16+12 ) );
-    
-    BufferOut.Store( DTid.x*16, asuint(i0 + i1) );
-    BufferOut.Store( DTid.x*16+4, asuint(f0 + f1) );
-    
-    uint dl, dh;
-    asuint( d0 + d1, dl, dh );
-
-    BufferOut.Store( DTid.x*16+8, dl );
-    BufferOut.Store( DTid.x*16+12, dh );
-#else
-    int i0 = Buffer0[DTid.x].i;
-    float f0 = Buffer0[DTid.x].f;
-    int i1 = Buffer1[DTid.x].i;
-    float f1 = Buffer1.[DTid.x].f;
-
-	BufferOut[DTid.x].i = i0 + i1;
-	BufferOut[DTid.x].f = f0 + f1;
-#endif // TEST_DOUBLE
+	
+	for (int m = 0; m < 256; m++)
+	{
+	    for (int n = 0; n < 9; n++)
+	    {
+		    for (int p = n; p < 9; p++)
+			{
+		        BufferOut[DTid.x * 256 + m].f[n] *= Buffer0[DTid.x *256 + m].f[p] * Buffer1[DTid.x *256 + m].f[p];
+			}
+		}
+	}
+	
+	for (int m = 0; m < 256; m++)
+	{
+	    for (int n = 0; n < 9; n++)
+	    {
+		    for (int p = n; p < 9; p++)
+			{
+		        BufferOut[DTid.x * 256 + m].f[n] *= Buffer0[DTid.x *256 + m].f[p] * Buffer1[DTid.x *256 + m].f[p];
+			}
+		}
+	}
 }
